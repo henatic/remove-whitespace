11eb81a6907bcadc15884ab113bf388980ffd581
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-04-14 16:02:38 -0700

    Closed files and tested Robots movie transcript.

--- redactor.py
+++ redactor.py
@@ -1,9 +1,10 @@
-f = open("Shrek_ScriptSPACED.txt" , "r")
+f = open("RobotsScriptSPACED.txt" , "r")
 
 lines = f.readlines()
-
-f = open('Shrek_Script.txt', 'w')
+f.close()
+f = open('RobotsScript.txt', 'w')
 
 for x in lines:
     if x.strip() != '':
         f.write(x)
+f.close()
